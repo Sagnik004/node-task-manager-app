5a380e5a9b8a77c77230c07e66dd7cb322c51a73
Sagnik004 <sagnikchakraborty910@yahoo.com>
2021-08-05 11:00:50 +0530

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,49 @@
+const mailgun = require('mailgun-js');
+
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+});
+
+const sendWelcomeEmail = (email, name) => {
+  const mailData = {
+    from: 'Task API <task_api@mailgun.org>',
+    to: email,
+    subject: 'Welcome to Task Manager App',
+    text: `
+${name},
+
+We welcome you to the task manager app.
+
+Please try it out and do let us know your feedback.`,
+  };
+
+  shootEmail(mailData);
+};
+
+const sendCancellationEmail = (email, name) => {
+  const mailData = {
+    from: 'Task API <task_api@mailgun.org>',
+    to: email,
+    subject: 'Task App - Signing out?',
+    text: `
+${name},
+
+We realized you have deleted your profile from Task App.
+
+Please let us know what went wrong!`,
+  };
+
+  shootEmail(mailData);
+};
+
+const shootEmail = (mailData) => {
+  mg.messages().send(mailData, (err, body) => {
+    console.log(body);
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancellationEmail,
+};
